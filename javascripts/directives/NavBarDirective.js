734b1d2d882026d3d422a07651d75b78ae928571
joechesney <33613119+joechesney@users.noreply.github.com>
2018-02-07 09:39:31 -0600

    got a test profile image and got a back button in the nav bar that works with window history

--- javascripts/directives/NavBarDirective.js
+++ javascripts/directives/NavBarDirective.js
@@ -6,6 +6,21 @@ module.exports =  function () {
   return {
     restrict: 'E',
     templateUrl: 'partials/navbar.html',
-    controller: 'NavBarCtrl'
+    controller: 'NavBarCtrl',
+    // template: '<button class="btn">{{back}}</button><button class="btn">{{forward}}</button>',
+    scope: {
+        back: '@back',
+        forward: '@forward',
+        icons: '@icons'
+    },
+    link: function($scope, element, attrs) {
+      // $(element[0]).on('click', function() {
+      //     history.back();
+      //     scope.$apply();
+      // });
+      document.getElementById('backButton').addEventListener("click", function($scope){
+        window.history.back();
+      });
+    }
   };
 };
