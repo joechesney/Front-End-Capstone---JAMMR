46cb5574c66305d2358bc1eb361dc7def32b1c09
joechesney <33613119+joechesney@users.noreply.github.com>
2018-02-21 12:57:26 -0600

    got info windows appearing. setting up actual info details inside of info window

--- javascripts/controllers/MapPageCtrl.js
+++ javascripts/controllers/MapPageCtrl.js
@@ -3,8 +3,19 @@
 
 // MESSAGES CTRL
 module.exports = function
-($scope, $location, AuthFactory, MessageFactory, $routeParams, ConversationFactory){
-
+($scope, $location, AuthFactory, MessageFactory, $routeParams, ConversationFactory, NgMap){
 
   
+  $scope.testUser = {
+    name: "Joe Dih",
+    age: 27,
+    guitar: true,
+    uid: "12345"
+  };
+  $scope.userArray = [$scope.testUser];
+
+  $scope.showDetails = function (e, userObj) {
+    $scope.selectedUser = userObj;
+    $scope.map.showInfoWindow("testMarker", userObj.uid);
+  };
 };
---
+++ javascripts/factories/MapFactory.js
@@ -0,0 +1,8 @@
+"use strict";
+
+
+module.exports = function($q, $http, fbConfig){
+  
+  
+  
+};
