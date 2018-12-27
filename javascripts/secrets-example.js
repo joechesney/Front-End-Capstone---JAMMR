5fd719fcd86fd42ed8047a09aa7a2139c2fec3f3
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-12-27 15:50:56 -0600

    added secrets and some media queries and deployed changes

---
+++ javascripts/secrets-example.js
@@ -0,0 +1,11 @@
+"use strict";
+const angular = require('angular');
+const app = angular.module('JAMMRApp');
+app.constant("fbConfig", {
+    apiKey: "123",
+    authDomain: "abc.com",
+    databaseURL: "abc.com",
+    projectId: "id",
+    storageBucket: "abc.com",
+    messagingSenderId: "123"
+  });
